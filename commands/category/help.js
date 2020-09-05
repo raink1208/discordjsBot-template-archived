fab2c1e80ed916407986b2f04115df4de7e7a839
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-09-05 20:53:00 +0900

    help commandの追加

--- commands/category/help.js
+++ commands/category/help.js
@@ -7,10 +7,12 @@ module.exports = {
     run: (client, message, args) => {
         let embed = new Discord.MessageEmbed()
             .setTitle("全コマンドリスト")
+
         client.commands.forEach((v,k) => {
             if (v.description === undefined) v.description = "説明なし"
             embed.addField(k,v.description)
         });
+
         message.channel.send(embed)
     }
 }
